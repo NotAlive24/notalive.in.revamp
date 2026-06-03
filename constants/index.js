2a84b6e9b6421aa5d46384a25052497f53bfd0cb
NotAlive24 <notalive7593@duck.com>
2026-06-03 10:13:21 +0530

    removed unused font

--- constants/index.js
+++ constants/index.js
@@ -1,8 +1,6 @@
 export const navLinks = [
     {   id : "about", title: "About Me"    },
     {   id : "achievements", title: "Achievements"    },
-    {   id : "isc2", title: "ISC2"    },
-    {   id : "py-roadmap", title: "Python Roadmap for Cyber"    },
-    {   id : "projects", title: "Projects"    },
+    {   id : "writeups", title: "Write-ups"    },
     {   id : "contact", title: "Contact"    }
 ]
